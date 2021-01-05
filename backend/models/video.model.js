7f23f6264bd60bcbaaa7a357dc10a316642db263
Nguyen Chuong <chunguyenchuong2014bg@gmail.com>
2021-01-05 09:41:03 +0700

    update section video and order

---
+++ backend/models/video.model.js
@@ -0,0 +1,13 @@
+// const mongoose = require("mongoose");
+
+// const videoSchema = new mongoose.Schema({
+//   courseId: {
+//     type: mongoose.Schema.ObjectId,
+//     ref : "Course",
+//     required : [true, "a video should be in 1 Course"]
+//   }
+// }, {
+//   timestamps: true,
+//   toJSON: { virtuals: true },
+//   toObject: { virtuals: true }
+// });
--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const { getAll, createOne, getOne, updateOne, deleteOne } = require('../controllers/order.controller');
-
+const {
+  requireSignin,
+  adminMiddleware,
+} = require("../controllers/auth.controller");
 router
   .route("/order")
   .get(getAll)
-  .post(createOne);
+  .post(requireSignin, adminMiddleware, createOne);
 
 router
   .route("/order/:slug")
   .get(getOne)
-  .patch(updateOne)
-  .delete(deleteOne)
+  .patch(requireSignin, adminMiddleware, updateOne)
+  .delete(requireSignin, adminMiddleware, deleteOne)
 
 module.exports = router;
